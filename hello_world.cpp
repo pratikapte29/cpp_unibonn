20a33b739eb6c9fde87b442ff5a1e57821c6768c
Pratik <pratikapte2911@gmail.com>
2026-04-21 10:22:15 +0200

    Implement add function

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+#include <iostream>  // preprocessor directive
+
+int addFunction(int a, int b){
+    return a + b;
+}
 
 int main(){
+    int a = 4;
+    int b = 8;
+    int c;
+
+    std::cout << "Value of " << "a is: " << a << std::endl;
+    std::cout << "Value of " << "b is: " << b << std::endl;
+
+    // add two numbers
+    c = addFunction(a, b);
+    std::cout << "Addition of " << a << " and " << b << " is: " << c << std::endl;
+
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
